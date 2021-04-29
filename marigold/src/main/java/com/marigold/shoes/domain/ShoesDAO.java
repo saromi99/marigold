7baa2bd124b8b3a3362014bc6ee1e248bf59ec21
sarom <sarom@DESKTOP-QVS7N4A>
2021-04-29 14:22:17 +0900

    DAO 수정

--- marigold/src/main/java/com/marigold/shoes/domain/ShoesDAO.java
+++ marigold/src/main/java/com/marigold/shoes/domain/ShoesDAO.java
@@ -1,5 +1,28 @@
 package com.marigold.shoes.domain;
 
+import java.util.List;
+
 public interface ShoesDAO {
+	public void insertShoes(ShoesVO vo);
+	public void updateShoes(ShoesVO vo);
+	public void deleteOneShoes(int article);
+	public void deleteAll();
+	public int getCount();
 	
+	public ShoesVO getOne(ShoesVO vo);
+	public List<ShoesVO> getList(ShoesVO vo);
+	
+	public List<ShoesVO> searchFirCategoryList(ShoesVO vo);
+	public List<ShoesVO> searchSecondCategoryList(ShoesVO vo);
+	public List<ShoesVO> searchThirdCategoryList(ShoesVO vo);
+	public List<ShoesVO> searchProductNameList(ShoesVO vo);
+	public List<ShoesVO> searchSpecialDayList(ShoesVO vo);
+	public List<ShoesVO> searchSeasonList(ShoesVO vo);
+	public List<ShoesVO> searchColorList(ShoesVO vo);
+	public List<ShoesVO> searchMaterialList(ShoesVO vo);
+	public List<ShoesVO> searchPriceList(ShoesVO vo);
+	public List<ShoesVO> searchHeelList(ShoesVO vo);
+	public List<ShoesVO> searchFootSizeList(ShoesVO vo);
+	public List<ShoesVO> searchBrandList(ShoesVO vo);
+	public List<ShoesVO> searchModelIdList(ShoesVO vo);
 }
--- marigold/src/main/java/com/marigold/shoes/impl/ShoesDAOImpl.java
+++ marigold/src/main/java/com/marigold/shoes/impl/ShoesDAOImpl.java
@@ -1,5 +1,132 @@
 package com.marigold.shoes.impl;
 
-public class ShoesDAOImpl {
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.marigold.shoes.domain.ShoesDAO;
+import com.marigold.shoes.domain.ShoesVO;
+
+@Repository("shoesDAO")
+public class ShoesDAOImpl implements ShoesDAO {
+
+	@Autowired
+	private SqlSession session;
+	
+	@Override
+	public void insertShoes(ShoesVO vo) {
+		session.insert("ShoesDAO.insertShoes", vo);
+	}
+
+	@Override
+	public void updateShoes(ShoesVO vo) {
+		session.update("ShoesDAO.updateShoes", vo);
+	}
+
+	@Override
+	public void deleteOneShoes(int article) {
+		session.delete("ShoesDAO.deleteOneShoes", article);
+	}
+
+	@Override
+	public void deleteAll() {
+		session.delete("ShoesDAO.deleteAllShoes");
+	}
+
+	@Override
+	public int getCount() {
+		return session.selectOne("ShoesDAO.getCount");
+	}
+
+	@Override
+	public List<ShoesVO> getList(ShoesVO vo) {
+		return session.selectList("ShoesDAO.getList", vo);
+	}
+
+	@Override
+	public ShoesVO getOne(ShoesVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoesVO> searchFirCategoryList(ShoesVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoesVO> searchSecondCategoryList(ShoesVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoesVO> searchThirdCategoryList(ShoesVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoesVO> searchProductNameList(ShoesVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoesVO> searchSpecialDayList(ShoesVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoesVO> searchSeasonList(ShoesVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoesVO> searchColorList(ShoesVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoesVO> searchMaterialList(ShoesVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoesVO> searchPriceList(ShoesVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoesVO> searchHeelList(ShoesVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoesVO> searchFootSizeList(ShoesVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoesVO> searchBrandList(ShoesVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoesVO> searchModelIdList(ShoesVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
